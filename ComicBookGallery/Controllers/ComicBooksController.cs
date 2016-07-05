3b4e881bee75d6f7682c669c5d18394e6f2c0cb9
Aaron Selonke <aarondv1@gmail.com>
2016-07-05 01:48:44 -0400

    Updated Comic Book Detailview to be stongly typed

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +12,26 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+            {
+                new Artist()  { Name="Dan Slott", Role="Script" },
+                new Artist()  { Role = "Pencils", Name="Humberto Ramos" },
+                new Artist()  { Name="Victor Olazaba", Role="Inks"},
+                new Artist()  { Name= "Edgar Delgado", Role="Colors"},
+                new Artist()  { Name="Chris Eliopoulous",Role="Letters"}
+            }
+
+
             };
 
-            return View();
+
+            //passing the comicbook instance into viewmethod call
+            return View(comicBook);
             
             }
         }
