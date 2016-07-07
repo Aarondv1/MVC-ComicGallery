980b29f22388d8f89698976c30dc9e3eb5c0b001
Aaron Selonke <aarondv1@gmail.com>
2016-07-07 00:11:23 -0400

    Updated comic books controller to use repository

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,28 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        //public ActionResult Detail()
-        //{
-            
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
 
 
-        //    //passing the comicbook instance into viewmethod call
-        //    //return View(comicBook);
+        public ActionResult Detail(int? id)
+        {
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            return View(comicBook);
+
+        
             
-        //    }
+           }
         }
     }
 
